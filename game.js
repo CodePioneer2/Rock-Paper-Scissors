a4ffe350c832b035d348c4c1d9122b97562d1ec3
CodePioneer2 <166389859+CodePioneer2@users.noreply.github.com>
2024-11-08 14:06:31 +0200

    finish game

--- game.js
+++ game.js
@@ -1,1 +1,55 @@
-console.log("Hello World")
+/* 
+1. Get Computer choice
+2. Get Human choice
+3. Play round
+4. Play game 5 rounds 
+*/
+
+const choices = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getHumanChoice() {
+  let playerChoice;
+  do {
+    playerChoice = prompt('Choose: Rock, Paper or Scissors').toLowerCase();
+  } while (!choices.includes(playerChoice));
+  return playerChoice;
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    let result = '';
+    switch (true) {
+      case computerChoice === humanChoice:
+        result = `It's tie
+        You both selected ${computerChoice}
+        Score: ${humanScore} - ${computerScore}`;
+        break;
+      case humanChoice === 'rock' && computerChoice === 'scissors':
+      case humanChoice === 'paper' && computerChoice === 'rock':
+      case humanChoice === 'scissors' && computerChoice === 'paper':
+        humanScore++;
+        result = `You win! 
+        You selected ${humanChoice} and computer selected ${computerChoice}.
+        Score: ${humanScore} - ${computerScore}`;
+        break;
+      default:
+        computerScore++;
+        result = `You lose!
+        You selected ${humanChoice} and computer selected ${computerChoice}.
+        Score: ${humanScore} - ${computerScore}`;
+        break;
+    }
+    return result;
+  }
+
+  while (humanScore < 5 && computerScore < 5) {
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+  }
+}
