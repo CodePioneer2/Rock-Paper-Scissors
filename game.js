5b92fc3ae448ed491adb0b73f2a8a976ecba01de
CodePioneer2 <166389859+CodePioneer2@users.noreply.github.com>
2024-11-11 01:47:53 +0200

    Update working with UI

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
 const choices = ['rock', 'paper', 'scissors'];
+const buttons = document.querySelectorAll('button');
+const resultDiv = document.querySelector('.result');
 
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    playRound(button.value, getComputerChoice());
+  });
+});
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
@@ -12,37 +19,42 @@ function getHumanChoice() {
   return playerChoice;
 }
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+let computerScore = 0;
+let humanScore = 0;
 
-  function playRound(humanChoice, computerChoice) {
-    let result = '';
-    switch (true) {
-      case computerChoice === humanChoice:
-        result = `It's tie
-        You both selected ${computerChoice}
-        Score: ${humanScore} - ${computerScore}`;
-        break;
-      case humanChoice === 'rock' && computerChoice === 'scissors':
-      case humanChoice === 'paper' && computerChoice === 'rock':
-      case humanChoice === 'scissors' && computerChoice === 'paper':
-        humanScore++;
-        result = `You win! 
-        You selected ${humanChoice} and computer selected ${computerChoice}.
-        Score: ${humanScore} - ${computerScore}`;
-        break;
-      default:
-        computerScore++;
-        result = `You lose!
-        You selected ${humanChoice} and computer selected ${computerChoice}.
-        Score: ${humanScore} - ${computerScore}`;
-        break;
-    }
-    return result;
+function playRound(humanChoice, computerChoice) {
+  let result = '';
+  resultDiv.innerHTML = '';
+  switch (true) {
+    case computerChoice === humanChoice:
+      result = `It's tie
+      You both selected ${computerChoice}
+      Score: ${humanScore} - ${computerScore}`;
+      break;
+    case humanChoice === 'rock' && computerChoice === 'scissors':
+    case humanChoice === 'paper' && computerChoice === 'rock':
+    case humanChoice === 'scissors' && computerChoice === 'paper':
+      humanScore++;
+      result = `You win! 
+      You selected ${humanChoice} and computer selected ${computerChoice}.
+      Score: ${humanScore} - ${computerScore}`;
+      break;
+    default:
+      computerScore++;
+      result = `You lose!
+      You selected ${humanChoice} and computer selected ${computerChoice}.
+      Score: ${humanScore} - ${computerScore}`;
+      break;
   }
 
-  while (humanScore < 5 && computerScore < 5) {
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
+  if (computerScore === 5) {
+    result += '<br><hr>Computer wins the game! Play again?';
+    computerScore = 0;
+    humanScore = 0;
+  } else if (humanScore === 5) {
+    result += '<br><hr>Human wins the game! Play again?';
+    computerScore = 0;
+    humanScore = 0;
   }
+  resultDiv.innerHTML = result;
 }
